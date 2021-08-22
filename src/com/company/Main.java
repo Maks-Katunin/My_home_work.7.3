e8faa568f9471f6369fe8c6c6e0a8b3400c2b90a
Maks-Katunin <katunin87@gmail.com>
2021-08-23 00:44:19 +0600

    heroes[i].applySuperAbility("?????");
     Я не понял как у меня это получилось...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         HavingSuperAbility[] heroes = {new Medic(), new Magic(),new Warrior()};
         for (int i = 0; i < heroes.length; i++) {
-            heroes[i].applySuperAbility();
+            heroes[i].applySuperAbility("?????");
 
         }
     }
